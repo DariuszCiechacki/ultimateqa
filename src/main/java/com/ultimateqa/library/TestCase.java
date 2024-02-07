dcc9cc3013bdc86b7b8cc0babd43cf56b4f6666d
ciechackidariusz <ciechacki.dariusz@gmail.com>
2024-02-07 18:18:11 +0100

    added test case methods

--- src/main/java/com/ultimateqa/library/TestCase.java
+++ src/main/java/com/ultimateqa/library/TestCase.java
@@ -1,4 +1,36 @@
 package com.ultimateqa.library;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public abstract class TestCase {
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        try{
+            // Chromedriver executable path
+            System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+
+            // WebDriver initialization
+            driver = new ChromeDriver();
+
+            // Set window dimension
+            driver.manage().window().setSize(new Dimension(1700, 1080));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Webdriver cannot be initialized");
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
