147a1887764953b7ec06ecb69b83d8a11b3a4723
Dariusz Ciechacki <ciechacki.dariusz@gmail.com>
2024-02-13 22:54:39 +0100

    added forms data

--- src/test/java/library/modules/automation/fillingoutforms/FormsData.java
+++ src/test/java/library/modules/automation/fillingoutforms/FormsData.java
@@ -1,4 +1,25 @@
 package library.modules.automation.fillingoutforms;
 
-public class FormsData {
+import library.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class FormsData extends BasePage {
+    public FormsData(WebDriver driver) {
+        super(driver);
+    }
+
+    public FormsData enterName(String name){
+        driver.findElement(By.xpath("//input[@data-original_id='name']"))
+                .sendKeys(name);
+
+        return this;
+    }
+
+    public FormsData enterMessage(String message){
+        driver.findElement(By.xpath("//textarea[@data-original_id='message']"))
+                .sendKeys(message);
+
+        return this;
+    }
 }
