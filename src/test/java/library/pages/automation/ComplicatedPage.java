acb620e030ba668f2713bd136df91ec82b49c004
Dariusz Ciechacki <ciechacki.dariusz@gmail.com>
2024-02-13 12:34:43 +0100

    added counting buttons in section of buttons method

--- src/test/java/library/pages/automation/ComplicatedPage.java
+++ src/test/java/library/pages/automation/ComplicatedPage.java
@@ -1,7 +1,13 @@
 package library.pages.automation;
 
 import library.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 public class ComplicatedPage extends BasePage {
     public ComplicatedPage(WebDriver driver) {
@@ -9,7 +15,15 @@ public class ComplicatedPage extends BasePage {
     }
 
     public ComplicatedPage waitForPageContent(){
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("Skills_Improved")));
 
         return this;
+    }
+
+    public int countButtonsInSectionOfButtons(){
+        List<WebElement> buttons = driver.findElements(
+                By.xpath("//div[contains(@class,'et_pb_row et_pb_row_2 et_pb_row_4col')]"));
+
+        return buttons.size();
     }
 }
