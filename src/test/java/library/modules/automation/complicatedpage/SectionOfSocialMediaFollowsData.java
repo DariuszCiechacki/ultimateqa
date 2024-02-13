d6c26611f628806c9fe72517d6f64c8c1adcd78e
Dariusz Ciechacki <ciechacki.dariusz@gmail.com>
2024-02-13 21:19:43 +0100

    added social media navigation links

--- src/test/java/library/modules/automation/complicatedpage/SectionOfSocialMediaFollowsData.java
+++ src/test/java/library/modules/automation/complicatedpage/SectionOfSocialMediaFollowsData.java
@@ -1,5 +1,8 @@
 package library.modules.automation.complicatedpage;
 
 public class SectionOfSocialMediaFollowsData {
+    protected static String twitterNavigationLink = "https://twitter.com/Nikolay_A00";
+    protected static String facebookNavigationLink = "https://www.facebook.com/Ultimateqa1/";
+
 
 }
