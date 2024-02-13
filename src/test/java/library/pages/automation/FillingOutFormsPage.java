6dfdf8c2feeeb0b6dbc5efc596be05906375c3ef
Dariusz Ciechacki <ciechacki.dariusz@gmail.com>
2024-02-13 22:48:17 +0100

    added fill all forms test class

--- src/test/java/library/pages/automation/FillingOutFormsPage.java
+++ src/test/java/library/pages/automation/FillingOutFormsPage.java
@@ -1,7 +1,9 @@
 package library.pages.automation;
 
 import library.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class FillingOutFormsPage extends BasePage {
     public FillingOutFormsPage(WebDriver driver) {
@@ -9,6 +11,8 @@ public class FillingOutFormsPage extends BasePage {
     }
 
     public FillingOutFormsPage waitForPageContent(){
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//textarea[@name='et_pb_contact_message_0']")));
 
         return this;
     }
