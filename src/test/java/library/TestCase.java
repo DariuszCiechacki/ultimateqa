76a052f2ff297217e5a89b7803922f8f84f272a3
Dariusz Ciechacki <ciechacki.dariusz@gmail.com>
2024-02-21 23:34:52 +0100

    added switch method for chrome, edge, firefox webdriver

--- src/test/java/library/TestCase.java
+++ src/test/java/library/TestCase.java
@@ -6,6 +6,8 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static library.Driver.driver;
 
@@ -13,8 +15,23 @@ public class TestCase {
 
     @BeforeClass
     public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser", "chrome");
+
+        switch (browser.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+        }
+
         driver.manage().window().setSize(new Dimension(1700, 1080));
         driver.get(ConfigLoader.getProperty("baseUrl"));
     }
